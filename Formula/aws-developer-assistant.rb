fc7678642810e176a1bfc51fe459cc25f4dd0467
Etch <etch@clearcutcoding.com>
2023-04-07 13:13:35 +0100

    bump 1.1.0

--- Formula/aws-developer-assistant.rb
+++ Formula/aws-developer-assistant.rb
@@ -1,7 +1,7 @@
 class AwsDeveloperAssistant < Formula
   desc "AWS Developer Assistant"
   homepage "https://github.com/ClearCutCoding/aws-developer-assistant"
-  url "https://github.com/ClearCutCoding/aws-developer-assistant/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/ClearCutCoding/aws-developer-assistant/archive/refs/tags/v1.1.0.tar.gz"
   sha256 "d9778b6b0fc40a4a5f64a134a2572e54016e13a888c3252a30645c71cf5236df"
   license ""
 
