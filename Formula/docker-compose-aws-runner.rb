1f79d5f43ddc7491fcc15c757bc28a27f9410f6d
Etch <etch@clearcutcoding.com>
2023-05-22 14:40:55 +0100

    bump: docker-compose-aws-runner -> v2.0.1

--- Formula/docker-compose-aws-runner.rb
+++ Formula/docker-compose-aws-runner.rb
@@ -1,8 +1,8 @@
 class DockerComposeAwsRunner < Formula
   desc "Docker Compose AWS Runner"
   homepage "https://github.com/ClearCutCoding/docker-compose-aws-runner"
-  url "https://github.com/ClearCutCoding/docker-compose-aws-runner/archive/refs/tags/v2.0.0.tar.gz"
-  sha256 "8e2a515306b055a31ea92ba1e79ee564d22fdef16f5ccd88306721386e86ee13"
+  url "https://github.com/ClearCutCoding/docker-compose-aws-runner/archive/refs/tags/v2.0.1.tar.gz"
+  sha256 "fc8e6bd1591ee5148c5395a567e23dff95b9ec26698f737a39368f3a91917558"
   license ""
 
   def install
