0c441b259bbb6e4ada15d8dff4c9ff34b678f87e
Etch <etch@clearcutcoding.com>
2023-05-22 14:33:04 +0100

    fix sha

--- Formula/docker-compose-aws-runner.rb
+++ Formula/docker-compose-aws-runner.rb
@@ -2,7 +2,7 @@ class DockerComposeAwsRunner < Formula
   desc "Docker Compose AWS Runner"
   homepage "https://github.com/ClearCutCoding/docker-compose-aws-runner"
   url "https://github.com/ClearCutCoding/docker-compose-aws-runner/archive/refs/tags/v2.0.0.tar.gz"
-  sha256 "3680ac2b3a0984e27d3c024abe343c42470f262ce67605570fb9c5ece42f4637"
+  sha256 "8e2a515306b055a31ea92ba1e79ee564d22fdef16f5ccd88306721386e86ee13"
   license ""
 
   def install
